2383796b5ee7314d80835f4587de567ecc620fb0
Weronika Slonina <weronika@pop-os.localdomain>
2023-05-12 20:54:04 +0200

    lekcja 1: zmienne

---
+++ lekcja_1_zmienne.py
@@ -0,0 +1,16 @@
+found_coins=20
+magic_coins=10
+stolen_coins=5
+weeks_per_year=52
+my_coins=(found_coins+
+   magic_coins*365-stolen_coins
+   *weeks_per_year)
+print(my_coins)
+bob="snhfsoivoesnvofseafvn"
+print(bob)
+alis='werwerwerwerwerwerwer'
+print(alis)
+web='''hi000000000000000
+iiiiiiiiiiiiii'''
+print(web)
+
